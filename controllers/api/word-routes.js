2281ef8f6bd54e6ec10b5bb1300c288c9242d478
Franklin <franklin.c.brown@gmail.com>
2022-03-29 07:52:39 -0700

    added config/connection, updated entry routes

--- controllers/api/word-routes.js
+++ controllers/api/word-routes.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 });
 
 // get one word
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => { //should this be '/:word'?
     Entry.findOne({
         where: {
             id: req.params.word //is this right? Since we aren't searching by id #?
@@ -38,6 +38,63 @@ router.get('/:id', (req, res) => {
             return;
         }
         res.json(dbEntryData);
+    });
+});
+
+router.post('/', (req, res) => {
+    // Expects { word: 'tintinabulation', definition: 'the sound of ringing bells' }
+    Entry.create({
+        word: req.body.word,
+        definition: req.body.definition
+    })
+    .then(dbEntryData => res.json(dbEntryData))
+    .catch(err => {
+        console.log(err);
+        res.json(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => { //again, should we search by word? Depends if the button is attached to the entry <card>
+    Entry.update(
+        {
+            word: req.params.word
+        },
+        {
+            definition: req.params.definition
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbEntryData => {
+        if (!dbEntryData) {
+            res.status(404).json({ message: 'No entry found!' });
+            return;
+        }
+        res.json(dbEntryData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Entry.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbEntryData => {
+        if (!dbEntryData) {
+            res.status(404).json({ message: 'No entry found!' });
+            return;
+        }
+        res.json(dbEntryData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
     });
 });
 
