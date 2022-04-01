f44dc7bbcbded2e6c569cdfe3791cf78a1a2cdff
Franklin <franklin.c.brown@gmail.com>
2022-04-01 06:22:09 -0700

    added models/index.js

--- models/index.js
+++ models/index.js
@@ -2,3 +2,13 @@ const User = require('./User');
 const Word = require('./Word');
 
 //create associations
+
+User.hasMany(Word, {
+    foreignKey: 'user_id'
+});
+
+Word.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Word };
