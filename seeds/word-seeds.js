0ccfe958bb6ad420b05cef218fea689f08c62ed7
Franklin <franklin.c.brown@gmail.com>
2022-04-06 19:25:57 -0700

    added seeds

---
+++ seeds/word-seeds.js
@@ -0,0 +1,63 @@
+const { Word } = require('../models');
+
+const wordData = [
+    {
+        word: 'twink',
+        definition: 'someone who looks like they are going to get broken in half by a light breeze',
+        user_id: 1
+    },
+    {
+        word:'tintinabulation',
+        definition:'the sound of the ringing of bells',
+        user_id: 2
+    },
+    {
+        word: 'floccinoccinihilipilification',
+        definition: 'the act of declaring something is worthless',
+        user_id: 3
+    },
+    {
+        word: 'mezzo',
+        definition: 'medium',
+        user_id: 4
+    },
+    {
+        word: 'anthurium',
+        definition:'an aroid with large, plasticky infloresences',
+        user_id: 5
+    },
+    {
+        word: 'glitter',
+        definition: 'the physical manifestation of dreams',
+        user_id: 1
+    },
+    {
+        word: 'ovine',
+        definition: 'pertaining to sheep',
+        user_id: 2
+    },
+    {
+        word: 'chemolithoautotroph',
+        definition:'these little pseudo-bacteria that live off of rock in the crust of the Earth',
+        user_id: 3
+    },
+    {
+        word:'terkw-',
+        definition: 'Proto-Indo-European root for turn',
+        user_id: 4
+    },
+    {
+        word: 'ballsy',
+        definition: 'risky, brave, confident',
+        user_id: 5
+    },
+    {
+        word: 'kriechenden Schnecken',
+        definition:'German for creeping snails',
+        user_id: 1
+    }
+];
+
+const seedWords = () => Word.bulkCreate(wordData);
+
+module.exports = seedWords;
