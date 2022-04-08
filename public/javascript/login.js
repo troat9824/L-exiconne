2c45353ec83435f968ee96cd7aff74cf51dae2be
Franklin <franklin.c.brown@gmail.com>
2022-04-08 07:52:15 -0700

    login and sign up bring us to main page, need to connect CSS

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector(/*'#email-login'*/).value.trim();
-    const password = document.querySelector(/*'#password-login'*/).value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
@@ -25,9 +25,9 @@ async function loginFormHandler(event) {
   async function signupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector(/*'#username-signup'*/).value.trim();
-    const email = document.querySelector(/*'#email-signup'*/).value.trim();
-    const password = document.querySelector(/*'#password-signup'*/).value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
