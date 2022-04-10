2db00fbbaa5613252a55f161bd424ff28930c5ee
Franklin <franklin.c.brown@gmail.com>
2022-04-09 19:53:38 -0700

    working on logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -97,7 +97,7 @@ router.post('/login', (req, res) => {
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
-      res.status(404).end();
+      res.status(204).end();
     });
   }
   else {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -83,4 +83,17 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
+
 module.exports = router;
