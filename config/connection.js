7806ff1f10e5b56963279854209cb3c62dd7f0cf
Franklin <franklin.c.brown@gmail.com>
2022-04-10 19:55:38 -0700

    update to connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   port: 3306
 }); */
 
-const { Sequelize } = require("sequelize/types");
+const { Sequelize } = require("sequelize");
 
 let sequelize;
 
